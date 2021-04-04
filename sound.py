aa1e0ce05b1ff56b73f600f52db9a3862fc4fc08
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-04-04 23:14:17 +0800

    Playing Audio Done

--- sound.py
+++ sound.py
@@ -95,5 +95,34 @@ class RecAUD:
         self.st = 0
 
 
+class PlayAudio:
+    def __init__(self, filepath, format_=pyaudio.paInt16, frames_per_buffer=2048):
+        self.filepath = filepath
+        self.format_ = format_
+        self.frames_per_buffer = frames_per_buffer
+        self.pyaudio_ = pyaudio.PyAudio()
+        self.device_settings = (
+            self.pyaudio_.get_default_input_device_info()
+        )  # Set Frames
+        self.input_device_index = self.device_settings["index"]
+        self.wave_file = wave.open(self.filepath)
+        self.output_device_info = self.pyaudio_.get_default_output_device_info()
+        self.output_device_index = self.output_device_info["index"]
+
+    def stream_out(self):
+        out = self.pyaudio_.open(
+            rate=self.wave_file.getframerate(),
+            channels=self.wave_file.getnchannels(),
+            format=self.format_,
+            output=True,
+            output_device_index=self.output_device_index,  # type: ignore
+        )
+        output_audio = self.wave_file.readframes(5 * self.wave_file.getframerate())
+        out.write(output_audio)
+
+
 if __name__ == "__main__":
-    guiAUD = RecAUD(filepath="./Audio/test_recording.wav")
+    FILEPATH = "./Audio/test_recording.wav"
+    # guiAUD = RecAUD(filepath=FILEPATH)
+    pa = PlayAudio(filepath=FILEPATH)
+    pa.stream_out()
